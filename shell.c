15f78e377dd2247e034b8b30371629f8b3342fba
2017029752 <suzybaek@hanyang.ac.kr>
2019-03-27 17:48:37 +0900

    2

--- shell.c
+++ shell.c
@@ -7,37 +7,41 @@
 #include <sys/wait.h>
 
 
-#define MAX_LINE 256
+#define MAX_LINE 1024
 
 int
 main(void)
 {
     char *buf[MAX_LINE];
-    char *argv[MAX_LINE];
-    char command[32];
+    char *arg[MAX_LINE];
     int proc_stat;
-    int n_Fork = 0;
-    int r_Process = 0;
-    char *p1;
-    char *p2;
+    char *p;
+    char *input[MAX_LINE];
 
 
     printf("prompt> ");
 
     while(1)
-    {
-        memset(buf, 0, MAX_LINE);
+    {   
+        int n_Fork = 0;
+        int r_Process = 0;
+        memset(buf, 0x00, MAX_LINE);
+        memset(input, 0x00, MAX_LINE);
+        memset(arg, 0x00, MAX_LINE);
+
         fgets(*buf, MAX_LINE-1, stdin);
         if(strcmp(*buf, "quit") == 0) break;
         
         buf[strlen(*buf)-1] = 0;
+        
+        p = strtok(*buf, ";");
+        input[n_Fork] = p;
 
-        p1 = strtok(*buf, ";");
-
-        while (p1 != NULL)
+        while (p != NULL)
         {
             n_Fork++;
-            p1 = strtok(NULL, ";");
+            p = strtok(NULL, ";");
+            input[n_Fork] = p;
         }
 
         pid_t pid[n_Fork];
@@ -45,20 +49,21 @@ main(void)
 
         while(r_Process < n_Fork)
         {
-            p2 = strtok(*buf, ";");
-            p1 = strtok(p2, " ");
-            p2[strlen(p2)-1] = 0;
-            if (p1 != NULL)
+            p = strtok(input[r_Process], " ");
+            p[strlen(p)-1] = '\0';
+            arg[0] = p;
+            int i = 0;
+            while(p != NULL)
             {
-                strcpy(command, p1);
-                strcpy(*argv, p2);
+                i++;
+                arg[i] = input[r_Process];
             }
 
             pid[r_Process] = fork();
 
             if(pid[r_Process] == 0)
             {
-                if(execvp(command, argv) == -1)
+                if(execvp(arg[0], arg) == -1)
                 {
                     printf("Failed to exec.\n");
                     exit(0);
